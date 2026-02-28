1e064b0a1ebaf394d11c4a384fb2b75d4c136327
Lyuben Kikov <exceptionhelper@gmail.com>
2026-02-28 17:01:23 +0200

    nothing new... ;/

---
+++ game/toggle.js
@@ -0,0 +1,12 @@
+const themeToggle = document.getElementById('theme-toggle');
+
+themeToggle.addEventListener('click', () => {
+    const html = document.documentElement;
+    const isDark = html.getAttribute('data-bs-theme') === 'dark';
+    
+    // Toggle the attribute
+    html.setAttribute('data-bs-theme', isDark ? 'light' : 'dark');
+    
+    // Update button text
+    themeToggle.innerText = isDark ? "DARK" : "LIGHT";
+});
